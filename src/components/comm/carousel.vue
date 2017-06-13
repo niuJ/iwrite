ac96bdbc6ead4b1336eb4f97755f7ebac68b9fe2
niuJ <417301532@qq.com>
2017-06-13 23:15:16 +0800

    20170613 0.01

---
+++ src/components/comm/carousel.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="carousel">
+        <transition-group tag='ul' class="clearfix slide" name='image'>
+            <li v-for='(image,index) in img' :key='index' v-show='index===mark'>
+                <a><img :src="image"></a>
+            </li>
+        </transition-group>
+        <div class="bullet">
+            <span v-for='(item,index) in img.length' :class="{'active':index===mark}" @click='change(index)'></span>
+        </div>
+    </div>
+</template>
+
+<script>
+     export default {
+       el: '.carousel',
+       data: {
+         mark: 0,
+         timer: null,
+         img: ['http://upload-images.jianshu.io/upload_images/3360875-5625658440cb542d.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+           'http://upload-images.jianshu.io/upload_images/3360875-b70e9d81d26e2a27.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+           'http://upload-images.jianshu.io/upload_images/3360875-dc724649454c2ddc.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+           'http://upload-images.jianshu.io/upload_images/3360875-d2148a1bb7ea9d21.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+         ]
+  },
+  created () {
+         this.play()
+  },
+  methods: {
+         change (i) {
+           this.mark = i
+         },
+         autoPlay () {
+           this.mark++
+           if (this.mark === 4) {
+             this.mark = 0
+             return
+           }
+         },
+         play () {
+           setInterval(this.autoPlay, 3000)
+         }
+  }
+}
+// http://www.jianshu.com/p/56fb1513a28a
+</script>
+
---
+++ src/views/iw-home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="iw-home">
+        <Carousel></Carousel>
+  </div>
+</template>
+
+<script>
+import Carousel from '../components/comm/carousel'
+
+export default {
+  name: 'iw-home',
+  components: {
+    Carousel
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
